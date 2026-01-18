1b06e1b47443adf3143f9033198947f2ed382774
leandro <leanaguirre2016@gmail.com>
2026-01-18 18:01:51 -0300

    config database - add dependencies - delete controller, service and spec app files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+    }),
+
+    MongooseModule.forRoot(process.env.MONGODB || '', {
+      dbName: 'gestorTurnos',
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
