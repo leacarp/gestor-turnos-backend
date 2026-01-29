1d7dbead2fe783aef0b9e42518e5f71dd9dfa2a7
Franco <francorodry15@gmail.com>
2026-01-29 08:20:42 -0300

    cambios pendientes al conectar ramas

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EnvConfiguration } from './config/env.config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -12,6 +13,10 @@ import { MongooseModule } from '@nestjs/mongoose';
     MongooseModule.forRoot(process.env.MONGODB || '', {
       dbName: 'gestorTurnos',
     }),
+
+    UserModule,
+
+   
   ],
   controllers: [],
   providers: [],
