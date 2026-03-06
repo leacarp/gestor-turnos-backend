52f50cda6490f19a38808256e00d4f6f27f1fbf9
Franco <francorodry15@gmail.com>
2026-03-06 11:27:36 -0300

    modulo actualizado con auth funcionando

--- src/availability/presentation/controllers/availability.controller.ts
+++ src/availability/presentation/controllers/availability.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Delete, Body, Param, Query, Inject, Put } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Body, Param, Query, Inject, Put, UseGuards } from '@nestjs/common';
 import type { IAvailabilityService } from '../../services/interfaces/IAvailabilityService';
 import { WeeklyScheduleEntity } from '../../domain/entities/weeklySchedule.entity';
 import { AvailabilityExceptionEntity } from '../../domain/entities/availabilityException.entity';
@@ -12,8 +12,15 @@ import { AvailableSlotResponseDto } from '../dto/availability-response/available
 import { WeeklyScheduleDtoService } from '../../services/dto/availability-dto.request/weeklySchedule-service.dto';
 import { AvailabilityExceptionDtoService } from '../../services/dto/availability-dto.request/availabilityException-service.dto';
 import { GetSlotsDtoService } from '../../services/dto/availability-dto.request/getSlots-service.dto';
+import { CurrentUser } from 'src/auth/presentation/decorators/current-user.decorator';
+import { AuthUserDto } from 'src/auth/presentation/dtos/auth-dto-response/auth-user.dto';
+import { JwtAuthGuard } from 'src/auth/infrastructure/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/infrastructure/guards/roles.guard';
+import { Roles } from 'src/auth/presentation/decorators/roles.decorator';
+
 
 @Controller('availability')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class AvailabilityController {
   constructor(
     @Inject('IAvailabilityService') private readonly availabilityService: IAvailabilityService,
@@ -21,32 +28,36 @@ export class AvailabilityController {
 
   // --- Weekly Schedule ---
   
-  @Post('schedule/:providerId')
-  async createSchedule(@Param('providerId') providerId: string, @Body() dto: WeeklyScheduleRequestDto): Promise<WeeklyScheduleResponseDto> {
-    const serviceDto = new WeeklyScheduleDtoService(providerId, dto.slots);
+  @Post('schedule')
+  @Roles('provider')
+  async createSchedule(@CurrentUser() user : AuthUserDto, @Body() dto: WeeklyScheduleRequestDto): Promise<WeeklyScheduleResponseDto> {
+    const serviceDto = new WeeklyScheduleDtoService(user.id, dto.slots);
     const entity = await this.availabilityService.createSchedule(serviceDto);
     return this.toWeeklyScheduleResponse(entity);
   }
     
-  @Get('schedule/:providerId')
-  async getSchedule(@Param('providerId') providerId: string): Promise<WeeklyScheduleResponseDto> {
-    const entity = await this.availabilityService.getSchedule(providerId);
+  @Get('schedule')
+  @Roles('provider')
+  async getSchedule(@CurrentUser() user : AuthUserDto): Promise<WeeklyScheduleResponseDto> {
+    const entity = await this.availabilityService.getSchedule(user.id);
     return this.toWeeklyScheduleResponse(entity);
   }
     
-  @Put('schedule/:providerId')
-  async updateSchedule(@Param('providerId') providerId: string,@Body() dto: WeeklyScheduleRequestDto): Promise<WeeklyScheduleResponseDto> {
-    const serviceDto = new WeeklyScheduleDtoService(providerId, dto.slots);
-    const entity = await this.availabilityService.updateSchedule(providerId, serviceDto);
+  @Put('schedule')
+  @Roles('provider')
+  async updateSchedule(@CurrentUser() user : AuthUserDto, @Body() dto: WeeklyScheduleRequestDto): Promise<WeeklyScheduleResponseDto> {
+    const serviceDto = new WeeklyScheduleDtoService(user.id, dto.slots);
+    const entity = await this.availabilityService.updateSchedule(user.id, serviceDto);
     return this.toWeeklyScheduleResponse(entity);
   }
     
     // --- Exceptions ---
     
-  @Post('exceptions/:providerId')
-  async createException(@Param('providerId') providerId: string, @Body() dto: AvailabilityExceptionRequestDto): Promise<AvailabilityExceptionResponseDto> {
+  @Post('exceptions')
+  @Roles('provider')
+  async createException(@CurrentUser() user : AuthUserDto, @Body() dto: AvailabilityExceptionRequestDto): Promise<AvailabilityExceptionResponseDto> {
     const serviceDto = new AvailabilityExceptionDtoService(
-        providerId,
+        user.id,
         new Date(dto.date + 'T00:00:00'),
         dto.type,
         dto.customSlots,
@@ -56,21 +67,23 @@ export class AvailabilityController {
     return this.toExceptionResponse(entity);
   }
     
-  @Get('exceptions/:providerId')
-  async getExceptionsByMonth(@Param('providerId') providerId: string, @Query('year') year: string, @Query('month') month: string): Promise<AvailabilityExceptionResponseDto[]> {
+  @Get('exceptions')
+  @Roles('provider')
+  async getExceptionsByMonth(@CurrentUser() user : AuthUserDto, @Query('year') year: string, @Query('month') month: string): Promise<AvailabilityExceptionResponseDto[]> {
     const entities = await this.availabilityService.getExceptionsByMonth(
-        providerId,
+        user.id,
         parseInt(year),
         parseInt(month),
     );
     return entities.map(entity => this.toExceptionResponse(entity));
   }
   
-  @Get('exceptions/:providerId/date')
-  async findExceptionByDate(@Param('providerId') providerId: string, @Query() query: GetExceptionByDateRequestDto): Promise<AvailabilityExceptionResponseDto | null> {
+  @Get('exceptions/date')
+  @Roles('provider')
+  async findExceptionByDate(@CurrentUser() user : AuthUserDto, @Query() query: GetExceptionByDateRequestDto): Promise<AvailabilityExceptionResponseDto | null> {
     const entity = await this.availabilityService.getExceptionsByDate(
-      providerId,
-      new Date(query.date),
+      user.id,
+      new Date(query.date + 'T00:00:00'),
     );
     return entity ? this.toExceptionResponse(entity) : null;
   }
@@ -78,6 +91,7 @@ export class AvailabilityController {
 
 
   @Delete('exceptions/:exceptionId')
+  @Roles('provider')
   async deleteException(@Param('exceptionId') exceptionId: string): Promise<{ message: string }> {
     await this.availabilityService.deleteException(exceptionId);
     return { message: 'Exception deleted successfully' };
@@ -86,6 +100,7 @@ export class AvailabilityController {
     // --- Available Slots ---
     
   @Get('slots/:providerId')
+  @Roles('client', 'provider', 'admin')
   async getAvailableSlots(@Param('providerId') providerId: string, @Query() query: GetSlotsRequestDto): Promise<AvailableSlotResponseDto[]> {
     const date = new Date(query.date + 'T00:00:00');
     const serviceDto = new GetSlotsDtoService(providerId, date);
